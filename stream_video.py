3c6a80c6a5b74baff8b0400d849d446b60461239
basameera <basameera@protonmail.com>
2020-11-21 21:00:59 +0100

    web

--- stream_video.py
+++ stream_video.py
@@ -6,8 +6,51 @@ import cv2
 app = Flask(__name__)
 
 # camera = cv2.VideoCapture('rtsp://freja.hiof.no:1935/rtplive/_definst_/hessdalen03.stream')
-camera = cv2.VideoCapture(0)
+# camera = cv2.VideoCapture(0)
 
+def gstreamer_pipeline(
+    capture_width=1280,
+    capture_height=720,
+    display_width=640,
+    display_height=360,
+    framerate=60,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, "
+        "format=(string)NV12, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+
+print(gstreamer_pipeline(flip_method=0))
+cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
+
+def show_camera():
+	# To flip the image, modify the flip_method parameter (0 and 2 are the most common)
+	
+	if cap.isOpened():
+		# Window
+		while True:
+			ret_val, frame = cap.read()
+			ret, buffer = cv2.imencode('.jpg', frame)
+			frame = buffer.tobytes()
+			yield (b'--frame\r\n'
+					b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+	else:
+		print("Unable to open camera")
 
 def gen_frames():  # generate frame by frame from camera
     while True:
@@ -25,7 +68,7 @@ def gen_frames():  # generate frame by frame from camera
 @app.route('/video_feed')
 def video_feed():
     # Video streaming route. Put this in the src attribute of an img tag
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(show_camera(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 @app.route('/')
