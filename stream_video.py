bbabb38c294ff9bfff5b41e7cc342e04c4b58fa9
basameera <basameera@protonmail.com>
2020-11-21 22:37:24 +0100

    stream video ready

--- stream_video.py
+++ stream_video.py
@@ -1,38 +1,80 @@
-from flask import Flask, render_template, Response
 import cv2
+import time
+import threading
+from flask import Response, Flask
 
-# https://github.com/NakulLakhotia/Live-Streaming-using-OpenCV-Flask
+# Image frame sent to the Flask object
+global video_frame
+video_frame = None
 
+# Use locks for thread-safe viewing of frames in multiple browsers
+global thread_lock
+thread_lock = threading.Lock()
+
+# GStreamer Pipeline to access the Raspberry Pi camera
+GSTREAMER_PIPELINE = 'nvarguscamerasrc ! video/x-raw(memory:NVMM), width=1280, height=720, format=(string)NV12, framerate=60/1 ! nvvidconv flip-method=0 ! video/x-raw, width=640, height=320, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink wait-on-eos=false max-buffers=1 drop=True'
+
+
+# Create the Flask object for the application
 app = Flask(__name__)
 
-camera = cv2.VideoCapture('rtsp://freja.hiof.no:1935/rtplive/_definst_/hessdalen03.stream')
-# camera = cv2.VideoCapture(0)
 
+def captureFrames():
+    global video_frame, thread_lock
 
-def gen_frames():  # generate frame by frame from camera
-    while True:
-        # Capture frame-by-frame
-        success, frame = camera.read()  # read the camera frame
-        if not success:
+    # Video capturing from OpenCV
+    video_capture = cv2.VideoCapture(GSTREAMER_PIPELINE, cv2.CAP_GSTREAMER)
+
+    while True and video_capture.isOpened():
+        return_key, frame = video_capture.read()
+        if not return_key:
             break
-        else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+
+        # Create a copy of the frame and store it in the global variable,
+        # with thread safe access
+        with thread_lock:
+            video_frame = frame.copy()
+
+        key = cv2.waitKey(30) & 0xff
+        if key == 27:  # Esc key to stop
+            break
+
+    video_capture.release()
 
 
-@app.route('/video_feed')
-def video_feed():
-    # Video streaming route. Put this in the src attribute of an img tag
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+def encodeFrame():
+    global thread_lock
+    while True:
+        # Acquire thread_lock to access the global video_frame object
+        with thread_lock:
+            global video_frame
+            if video_frame is None:
+                continue
+            return_key, encoded_image = cv2.imencode(".jpg", video_frame)
+            if not return_key:
+                continue
+
+        # Output image as a byte array
+        yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
+              bytearray(encoded_image) + b'\r\n')
 
 
-@app.route('/')
-def index():
-    """Video streaming home page."""
-    return render_template('index.html')
+@app.route("/")
+def streamFrames():
+    return Response(encodeFrame(), mimetype="multipart/x-mixed-replace; boundary=frame")
 
 
+# check to see if this is the main thread of execution
 if __name__ == '__main__':
-    app.run(debug=True)
+
+    # Create a thread and attach the method that captures the image frames, to it
+    process_thread = threading.Thread(target=captureFrames)
+    process_thread.daemon = True
+
+    # Start the thread
+    process_thread.start()
+
+    # start the Flask Web Application
+    # While it can be run on any feasible IP, IP = 0.0.0.0 renders the web app on
+    # the host machine's localhost and is discoverable by other machines on the same network
+    app.run("0.0.0.0", port="8000")
